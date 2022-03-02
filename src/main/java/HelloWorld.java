c722f2a34b432eeeab2b7709cd5466a7f1c15d1e
Edgar Bagagem <2002edgargomes@gmail.com>
2022-03-02 10:02:10 +0000

    adicionar o meu nome

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -3,6 +3,7 @@ import javax.swing.*;
 public class HelloWorld extends JFrame {
     private JPanel panel1;
     private JLabel lblMensagem;
+    private JLabel lblMensagem2;
 
     public HelloWorld(){
         setContentPane(panel1);
